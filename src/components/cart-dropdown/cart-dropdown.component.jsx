582cc881255ba6099da73c99fa5bb0771b58e937
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2024-01-15 17:43:38 +0100

    Updated cart dropdown component to display empty message when cart is empty and adjusted button styles

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,10 +3,10 @@ import {CartItem} from "../cart-item/cart-item.component.jsx";
 import {useContext} from "react";
 import {CartContext} from "../../contexts/cart.context.jsx";
 import {useNavigate} from "react-router-dom";
-import {CartDropdownContainer, CartItems} from "./cart-dropdown.styles.jsx";
+import {CartDropdownContainer, CartItems, EmptyMessage} from "./cart-dropdown.styles.jsx";
 
 const CartDropdown = () => {
-  const { cartItems } = useContext(CartContext)
+  const {cartItems} = useContext(CartContext)
   const navigate = useNavigate()
 
   const gotoToCheckoutHandler = () => navigate("/checkout")
@@ -14,7 +14,10 @@ const CartDropdown = () => {
   return (
     <CartDropdownContainer>
       <CartItems>
-        {cartItems.map(item => <CartItem key={item.id} cartItem={item}/>)}
+        {
+          cartItems.length ? cartItems.map(item => <CartItem key={item.id} cartItem={item}/>)
+            : <EmptyMessage>Your cart is empty</EmptyMessage>
+        }
       </CartItems>
       <Button onClick={gotoToCheckoutHandler}>GO TO CHECKOUT</Button>
     </CartDropdownContainer>
