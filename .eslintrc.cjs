9b9e9cc310541a77b5e999bdecc517e23133b149
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2024-01-18 06:28:44 +0100

    Added Prettier and updated ESLint configuration for improved code formatting and linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,25 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended'
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "react/react-in-jsx-scope": "off",
   },
-}
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      babelrc: false,
+      configFile: false,
+      presets: ["@babel/preset-env", "@babel/preset-react"],
+    },
+  },
+};
