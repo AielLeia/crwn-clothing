c436da849ba00937cabfdd5f0f2311ee1a49baab
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2024-01-19 08:12:28 +0100

    Refactored categories fetching to use Redux Thunk for async actions, added loading and error handling to categories state, and updated related actions and reducers

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,19 +4,13 @@ import { useEffect } from "react";
 
 import { CategoriesPreview } from "../categories-preview/categories-preview.component.jsx";
 import { Category } from "../category/category.component.jsx";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.util.js";
-import { setCategories } from "../../store/categories/categories.action.js";
+import { fetchCategoriesAsync } from "../../store/categories/categories.action.js";
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categories = await getCategoriesAndDocuments();
-      dispatch(setCategories(categories));
-    };
-
-    getCategoriesMap().then((r) => console.log(r));
+    dispatch(fetchCategoriesAsync());
   }, []);
 
   return (
--- src/store/categories/categories.action.js
+++ src/store/categories/categories.action.js
@@ -1,5 +1,25 @@
 import { createAction } from "../../utils/reducer/reducer.util.js";
 import { CATEGORIES_ACTION_TYPES } from "./categories.types.js";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.util.js";
 
-export const setCategories = (categories) =>
-  createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categories);
+export const fetchCategoriesStart = () =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (categories) =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categories);
+
+export const fetchCategoriesFailed = (error) =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+  dispatch(fetchCategoriesStart());
+
+  try {
+    const categories = await getCategoriesAndDocuments();
+    dispatch(fetchCategoriesSuccess(categories));
+  } catch (e) {
+    dispatch(fetchCategoriesFailed(e));
+  }
+};
+
+export class setCategories {}
